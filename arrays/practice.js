621b38857aee7ee48e9fea32da7fa1be0d0a8f98
CoderBritt89 <72882552+CoderBritt89@users.noreply.github.com>
2021-01-07 18:44:52 -0700

    finished

--- arrays/practice.js
+++ arrays/practice.js
@@ -91,15 +91,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 const evenFinder = (nums) => {
 
   const evenNums = [];
-
-  for (i=0; i< nums.length; i++) {
+ 
+  for (i=0; i< nums.length; i++) {  
     if ((nums[i] %2) ===0 ){
       evenNums.push(nums[i])
-
-      return evenNums;
+     
     } 
   }
-
+  return evenNums;
 
 
 }
